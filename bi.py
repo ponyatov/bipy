4608db427523d14694357786e1428ee8cc2283d4
Dmitry Ponyatov <dponyatov@gmail.com>
2019-09-20 10:49:14 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- bi.py
+++ bi.py
@@ -1,9 +1,16 @@
 import os,sys
 
 try:
-    SRC = open(sys.argv[1],'rb').read()
+    INF = sys.argv[1]
+    SRC = open(INF,'rb').read()
 except IndexError:
-    SRC = open(sys.argv[0],'rb').read()
+    INF = sys.argv[0]
+    SRC = open(INF,'rb').read()
+
+sys.stdout = log = open(INF+'.log','w')
+print 'BY:',sys.argv[0]
+print 'INF:',INF
 
 import ply.lex  as lex
 import ply.yacc as yacc
+
